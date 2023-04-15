e1103f253cba476c91e8a0fdd052e648da2ae53a
Pera Urosevic <pera.urosevic@gmail.com>
2023-04-15 08:56:14 +0300

    improve pile card

--- lib/widgets/pile.dart
+++ lib/widgets/pile.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hoard/providers/hoard_provider.dart';
-import 'package:hoard/services/data_service.dart';
 import 'package:hoard/services/ui_service.dart';
 import 'package:hoard/widgets/artifact.dart';
 import 'package:hoard/widgets/pile/pile_bottom_bar.dart';
 import 'package:hoard/providers/artifact_provider.dart';
+import 'package:hoard/widgets/pile/pile_card.dart';
 import 'package:hoard/widgets/prompt.dart';
 import 'package:provider/provider.dart';
 import 'package:hoard/widgets/pile/pile_editor.dart';
@@ -79,53 +79,7 @@ class _PileState extends State<Pile> {
         child: ListView.builder(
           itemCount: providerPile.artifacts.length,
           itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                contentPadding: const EdgeInsetsDirectional.symmetric(horizontal: 8, vertical: 6),
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 6),
-                  child: Text(
-                    providerPile.artifacts[index]['id'],
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ),
-                subtitle: Table(
-                  columnWidths: const {
-                    0: IntrinsicColumnWidth(),
-                    1: FlexColumnWidth(1),
-                  },
-                  children: providerPile.fieldsIndex
-                      .where((field) => field.id != 'id')
-                      .map((field) => TableRow(
-                            children: [
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.fromLTRB(0, 2, 8, 2),
-                                  child: Text(
-                                    titleCase(field.id),
-                                    style: TextStyle(color: Colors.grey.shade500),
-                                  ),
-                                ),
-                              ),
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.fromLTRB(0, 2, 0, 2),
-                                  child: Text(
-                                    providerPile.artifacts[index][field.id].toString(),
-                                    style: TextStyle(color: Colors.grey.shade500),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ))
-                      .toList(),
-                ),
-                onTap: () {
-                  providerArtifact.load(providerPile, providerPile.artifacts[index]);
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const Artifact()));
-                },
-              ),
-            );
+            return PileCard(index);
           },
         ),
       ),
---
+++ lib/widgets/pile/pile_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:hoard/models/field_model.dart';
+import 'package:hoard/widgets/artifact.dart';
+import 'package:provider/provider.dart';
+import 'package:hoard/providers/pile_provider.dart';
+import 'package:hoard/providers/artifact_provider.dart';
+import 'package:hoard/services/data_service.dart';
+
+class PileCard extends StatelessWidget {
+  final int index;
+  const PileCard(this.index, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    PileProvider providerPile = context.watch<PileProvider>();
+    ArtifactProvider providerArtifact = context.read<ArtifactProvider>();
+
+    List<FieldModel> subtitleFields = providerPile.fieldsIndex.where((field) => field.id != 'id').toList();
+
+    return Card(
+      child: ListTile(
+        contentPadding: const EdgeInsetsDirectional.symmetric(horizontal: 16, vertical: 6),
+        title: Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, subtitleFields.isNotEmpty ? 4 : 0),
+          child: Text(
+            providerPile.artifacts[index]['id'],
+            style: const TextStyle(fontSize: 18),
+          ),
+        ),
+        subtitle: subtitleFields.isNotEmpty
+            ? Table(
+                columnWidths: const {
+                  0: IntrinsicColumnWidth(),
+                  1: FlexColumnWidth(1),
+                },
+                children: subtitleFields
+                    .map(
+                      (field) => TableRow(
+                        children: [
+                          TableCell(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 2, 8, 2),
+                              child: Text(
+                                titleCase(field.id),
+                                style: TextStyle(color: Colors.grey.shade500),
+                              ),
+                            ),
+                          ),
+                          TableCell(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                              child: Text(
+                                providerPile.artifacts[index][field.id].toString(),
+                                style: TextStyle(color: Colors.grey.shade500),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              )
+            : null,
+        onTap: () {
+          providerArtifact.load(providerPile, providerPile.artifacts[index]);
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const Artifact()));
+        },
+      ),
+    );
+  }
+}
