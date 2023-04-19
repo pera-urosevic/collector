36fce7c4c7e8e2c541aa66528e3e8eb081bde58d
Pera Urosevic <pera.urosevic@gmail.com>
2023-04-19 20:40:49 +0300

    escape shortcut

--- lib/widgets/pile.dart
+++ lib/widgets/pile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hoard/providers/hoard_provider.dart';
 import 'package:hoard/services/ui_service.dart';
 import 'package:hoard/widgets/artifact.dart';
@@ -24,88 +25,98 @@ class _PileState extends State<Pile> {
     PileProvider providerPile = context.watch<PileProvider>();
     ArtifactProvider providerArtifact = context.read<ArtifactProvider>();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('${providerPile.id} (${providerPile.artifacts.length})'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () async {
-              showDialog(
-                useRootNavigator: false,
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Confirmation'),
-                    content: const Text('Are you sure you want to remove?'),
-                    actions: [
-                      TextButton(
-                        child: const Text('OK'),
-                        onPressed: () async {
-                          String pileId = providerPile.id;
-                          await providerHoard.removePile(pileId);
-                          if (!mounted) return;
-                          displayMessage(context, 'Removed "$pileId"');
-                          Navigator.of(context).pop();
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      TextButton(
-                        child: const Text('Cancel'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
+    return CallbackShortcuts(
+      bindings: {
+        const SingleActivator(LogicalKeyboardKey.escape): () {
+          Navigator.of(context).pop();
+        },
+      },
+      child: Focus(
+        autofocus: true,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('${providerPile.id} (${providerPile.artifacts.length})'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () async {
+                  showDialog(
+                    useRootNavigator: false,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Confirmation'),
+                        content: const Text('Are you sure you want to remove?'),
+                        actions: [
+                          TextButton(
+                            child: const Text('OK'),
+                            onPressed: () async {
+                              String pileId = providerPile.id;
+                              await providerHoard.removePile(pileId);
+                              if (!mounted) return;
+                              displayMessage(context, 'Removed "$pileId"');
+                              Navigator.of(context).pop();
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: const Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
                   );
                 },
+              ),
+              const SizedBox(width: 12),
+              IconButton(
+                icon: const Icon(Icons.edit),
+                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const PileEditor())),
+              ),
+              IconButton(
+                icon: const Icon(Icons.refresh),
+                onPressed: () => context.read<PileProvider>().loadPile(providerPile.id),
+              ),
+              const SizedBox(width: 8)
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 56),
+            child: ListView.builder(
+              itemCount: providerPile.artifacts.length,
+              itemBuilder: (context, index) {
+                return PileCard(index);
+              },
+            ),
+          ),
+          bottomSheet: PileBottomBar(providerPile: providerPile),
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () async {
+              String newArtifactId = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Prompt(
+                    title: 'Create new artifact',
+                    hint: 'Artifact ID',
+                    valid: (value) => value.isNotEmpty,
+                  ),
+                ),
+              );
+              providerArtifact.create(newArtifactId, providerPile);
+              if (!mounted) return;
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Artifact(),
+                ),
               );
             },
           ),
-          const SizedBox(width: 12),
-          IconButton(
-            icon: const Icon(Icons.edit),
-            onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const PileEditor())),
-          ),
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: () => context.read<PileProvider>().loadPile(providerPile.id),
-          ),
-          const SizedBox(width: 8)
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 8, 8, 56),
-        child: ListView.builder(
-          itemCount: providerPile.artifacts.length,
-          itemBuilder: (context, index) {
-            return PileCard(index);
-          },
         ),
-      ),
-      bottomSheet: PileBottomBar(providerPile: providerPile),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () async {
-          String newArtifactId = await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Prompt(
-                title: 'Create new artifact',
-                hint: 'Artifact ID',
-                valid: (value) => value.isNotEmpty,
-              ),
-            ),
-          );
-          providerArtifact.create(newArtifactId, providerPile);
-          if (!mounted) return;
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const Artifact(),
-            ),
-          );
-        },
       ),
     );
   }
